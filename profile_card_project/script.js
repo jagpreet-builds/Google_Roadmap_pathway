c99e3707e7a42b11b3d170bc33b2d6c92184c6e0
Jagpreet Singh <jagpreet.uow@gmail.com>
2026-05-28 23:05:36 +1000

    add js for button click started learning JS

---
+++ profile_card_project/script.js
@@ -0,0 +1,15 @@
+let myName= "Jagpreet Singh";
+let myRole = "AI-Developer";
+let mySkill = "Javascript";
+
+console.log(myName);
+console.log(myRole);
+
+
+function changePosition(){
+    document.getElementById("position").innerHTML = "Project Manager at Google"
+}
+
+function showContact(){
+    document.getElementById("Contact").innerHTML = "Call me at 0405351834"
+}
